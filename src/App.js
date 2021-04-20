f3349efdb3e857aa6b1b06d979528e40d81a1122
GersonDantas <gerson.accari@hotmail.com>
2021-04-20 12:57:30 -0300

    webpack wich ReactJS

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <p>Digital Inovation one</p>
+      <p>Bem vindo doidão</p>
+    </div>
+  );
+};
+
+export default App;
